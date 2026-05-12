4e2fa6641fb4278d6eb05b20827336d423a9bee1
Dang Luu Anh Tu <anhtu29126@gmail.com>
2026-05-12 21:39:38 +0700

    Implement basic ALU execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include "../include/instruction.h"
+#include "../include/alu.h"
 
 int main(){
     Instruction inst;
@@ -14,5 +15,8 @@ int main(){
     printf("Opcode: %d\n", inst.opcode);
     printf("rd: %d, rs1: %d, rs2: %d\n", inst.rd, inst.rs1, inst.rs2);
 
+    int result = execute(inst, 10, 5);
+    printf("Result: %d\n", result);
+    
     return 0;
 }
